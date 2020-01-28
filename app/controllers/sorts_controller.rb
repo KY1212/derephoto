48232092eb05392f5646265d00909f8c38600711
KY1212 <YumYum1212@docomo.ne.jp>
2020-01-29 02:52:32 +0900

    a

--- app/controllers/sorts_controller.rb
+++ app/controllers/sorts_controller.rb
@@ -9,10 +9,7 @@ class SortsController < ApplicationController
   private
 
   def image_params
-<<<<<<< HEAD
     params.fetch(:image, {}).permit(:name,, :comment, :idolname, :idoltype, :mv, :avatar )
-=======
     params.fetch(:image, {}).permit(:name, :comment, :idolname, :idoltype, :mv, :avatar )
->>>>>>> sort
   end
 end
